12227732bfe000229d92a6368b1ef2b8bf0f53ba
Toni Hermoso Pulido <toniher@cau.cat>
2015-01-09 12:36:22 +0100

    magic word

--- Aform.i18n.magic.php
+++ Aform.i18n.magic.php
@@ -6,6 +6,7 @@ $magicWords = array();
 $magicWords['en'] = array(
 	'aform'    => array( 0, 'aform' ),
 	'ainput'    => array( 0, 'ainput' ),
+	'ainput_multi'    => array( 0, 'ainput_multi' ),
 	'aselect'    => array( 0, 'aselect' ),
 	'alabel'    => array( 0, 'alabel' ),
 	'aformend'    => array( 0, 'aformend' )
